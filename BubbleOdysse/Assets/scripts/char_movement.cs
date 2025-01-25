be78d1fe5b9734f253da5594d657c63e9b06aeff
GypsyHill <146959173+GypsyHill@users.noreply.github.com>
2025-01-25 14:10:33 +0100

    Actions

--- BubbleOdysse/Assets/scripts/char_movement.cs
+++ BubbleOdysse/Assets/scripts/char_movement.cs
@@ -6,14 +6,23 @@ using UnityEngine.InputSystem;
 public class char_movement : MonoBehaviour
 {
 
+    [Header("References")]
     [SerializeField] private Camera cam;
     private CharacterController controller;
+    [SerializeField] private GameObject bubble;
 
-    [SerializeField] private float moveSpeed;
-    
-    private float gravity = 9.8f;
 
+    [Header("Movement Variables")]
     private Vector2 moveInput;
+    [SerializeField] private float dashForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float gravity = 9.8f;
+    private bool isGrounded;
+
+
+    private int mode = 0;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +34,7 @@ public class char_movement : MonoBehaviour
     void Update()
     {
         Move();
+        GroundCheck();
     }
 
     private void Move()
@@ -46,4 +56,72 @@ public class char_movement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
     }
+
+    public void SwitchMode(InputAction.CallbackContext context)
+    {
+        //switch between modes and change the mode variable and move speed
+
+        //Modes: 0 - Dash, 1 - Floating, 2 - SummonBubble
+        
+        mode = (mode + 1) % 3;
+        if(mode == 0){
+            moveSpeed = 10;
+        }
+        else if(mode == 1){
+            moveSpeed = 3;
+        }
+        else if(mode == 2){
+            moveSpeed = 5;
+        }      
+
+    }
+    
+    public void ActionInput(InputAction.CallbackContext context)
+    {
+
+        //Check which mode is active and call the corresponding function
+        if(mode == 0){
+            Dash();
+        }
+        else if(mode == 1){
+            Floating();
+        }
+        else if(mode == 2){
+            SummonBubble();
+        }
+            
+    
+    }
+    public void Jump(InputAction.CallbackContext context){
+        //Lets the player jump normaly
+
+    }
+
+    private void Floating(){
+        // Lets the player float in the air while holding spacbar and move slowly in the air
+    }
+
+    private void Dash(){
+        //Lets the player dash in the direction he is facing
+        controller.Move(transform.forward * dashForce * Time.deltaTime);
+
+
+    }
+
+    private void SummonBubble(){
+        //Check if the bubble is already summoned
+             
+
+        //Summon the bubble in front of the player
+
+        //bubble functions as a platform that the player can jump on
+        
+
+    }
+
+    private void GroundCheck(){
+        // Check if the player is standing on the ground
+        
+    }
+
 }
